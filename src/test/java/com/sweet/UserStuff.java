49e6333872214bda80f111fbe46be27cfb114f8c
Qais <hweidiqais@pm.me>
2024-07-30 23:49:05 +0300

    just a test

--- src/test/java/com/sweet/UserStuff.java
+++ src/test/java/com/sweet/UserStuff.java
@@ -3,7 +3,6 @@ package com.sweet;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.Test;
 
 public class UserStuff {
 
