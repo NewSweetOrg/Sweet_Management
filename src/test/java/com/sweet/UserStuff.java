c38ed07e2c67ecc3421d32c36a5314a23e047e6c
Qais <hweidiqais@pm.me>
2024-07-31 11:59:12 +0300

    just a test

--- src/test/java/com/sweet/UserStuff.java
+++ src/test/java/com/sweet/UserStuff.java
@@ -10,6 +10,7 @@ public class UserStuff {
     public void i_am_on_the_sign_up_step() {
         // Write code here that turns the phrase above into concrete actions
         //tst
+        //sts
         //throw new io.cucumber.java.PendingException();
     }
     @When("when the user enters a username {string} and password {string}")
